2fbfcc7d7ce34788754f4d50288e3eff5eb79ebc
Jose Lara <joselara@wposs.com>
2022-02-05 23:09:06 -0300

    R001_INICIO_ESQUEMA_APP
    * Primer commit
    * Incio esquema app

---
+++ app/src/main/java/com/wposs/danko/activity/ActivityLogin.java
@@ -0,0 +1,16 @@
+package com.wposs.danko.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.wposs.danko.R;
+
+public class ActivitySplash  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/wposs/danko/activity/ActivitySplash.java
@@ -0,0 +1,2 @@
+package com.wposs.danko.activity;public class ActivitySplash {
+}
